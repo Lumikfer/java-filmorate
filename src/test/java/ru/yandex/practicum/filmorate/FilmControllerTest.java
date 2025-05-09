497826f374838d937cdada7b4a053af9deff2d25
zumikfer <zumikfer21@yandex.ru>
2025-05-09 16:52:22 +0300

    update

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -1,16 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import jakarta.validation.ValidationException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.io.IOException;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class FilmControllerTest {
 
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -1,10 +1,4 @@
 package ru.yandex.practicum.filmorate;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.model.User;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
 
