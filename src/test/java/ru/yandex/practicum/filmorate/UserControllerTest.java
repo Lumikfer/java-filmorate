6fc62dd2a8a24834198db26571d6277a0d8dad4c
zumikfer <zumikfer21@yandex.ru>
2025-05-09 16:23:14 +0300

    update

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.Collection;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
