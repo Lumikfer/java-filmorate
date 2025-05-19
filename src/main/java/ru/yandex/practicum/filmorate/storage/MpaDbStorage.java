cbd0026669af9efc428a7a45054c3049e1a88cf3
zumikfer <zumikfer21@yandex.ru>
2025-05-19 21:03:50 +0300

    add-database

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaDbStorage.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.storage;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
