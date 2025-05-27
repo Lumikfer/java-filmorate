16b8a72726bf94fc04cdc020a6a9fedc2f876e53
Ilnar <safiullinko@yandex.ru>
2025-05-27 22:56:02 +0300

    убрать пустые аннотации

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -103,7 +103,7 @@ public class FilmService {
     public List<Film> getCommonFilms(int userId,int friendId) {
         List<Film> commonFilms = new ArrayList<>();
         List<Film> allfilm = new ArrayList<>(filmStorage.getFilms());
-        for(Film film : allfilm) {
+        for (Film film : allfilm) {
             if (film.getLike().contains(userId) && film.getLike().contains(friendId)) {
                 commonFilms.add(film);
             }
