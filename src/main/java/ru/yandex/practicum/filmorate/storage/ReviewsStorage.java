1f56ae5844ae7b5909368811d7175f72d9352f9f
zumikfer <zumikfer21@yandex.ru>
2025-05-29 23:37:37 +0300

    update

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewsStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewsStorage.java
@@ -1,15 +1,23 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Reviews;
+
 import java.util.List;
 
 public interface ReviewsStorage {
     List<Reviews> getRew();
+
     Reviews getRewById(int id);
+
     void addRew(Reviews review);
+
     void delRewById(Reviews review);
+
     void addUseful(int reviewId, int userId);
+
     void delUseful(int reviewId, int userId);
+
     void updateRew(Reviews review);
+
     List<Reviews> getReviewsByFilmId(int filmId);
 }
