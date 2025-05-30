64b062690768ceb62fdb25cfdee55111be810aba
zumikfer <zumikfer21@yandex.ru>
2025-05-30 23:11:41 +0300

    update

--- src/main/java/ru/yandex/practicum/filmorate/model/Reviews.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Reviews.java
@@ -5,9 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Data
 @Builder
 public class Reviews {
